e322870d55c05efbd1cb9c8a1f70b6a3af13e1ed
Daren Morreale <daren@givelegacy.com>
2025-03-22 12:02:58 -0500

    Firsty commit

---
+++ src/components/AllPlayers.jsx
@@ -0,0 +1,7 @@
+export default function AllPlayers() {
+  return (
+    <div>
+      <h1>All Players</h1>
+    </div>
+  );
+}
---
+++ src/components/NewPlayer.jsx
@@ -0,0 +1,7 @@
+export default function NewPlayer() { 
+  return (
+    <div>
+      <h1>New Player</h1>
+    </div>
+  );
+}
